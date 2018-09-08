15f763e84e04a7c3c9dfcd3af7a01e30c607e9ce
平兄 <597914752@qq.com>
2018-09-09 01:16:07 +0800

    插入贴吧logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <baidu-map class="bm-view" id="bm-div" :center="{lng: 116.404, lat: 39.915}" :zoom="15" :scroll-wheel-zoom="true">
+    <baidu-map class="bm-view" id="bm-div" center="柳州市广西科技大学" :zoom="15" :scroll-wheel-zoom="true">
+        <bm-copyright style="display: block"
+                anchor="BMAP_ANCHOR_TOP_RIGHT"
+                :copyright="[{id: 1, content: '<a href=\'http://tieba.baidu.com/f?kw=%B9%E3%CE%F7%BF%C6%BC%BC%B4%F3%D1%A7&fr=ala0&tpl=5\'><img src=\'./tieba2.png\' height=\'46px\'></a>'}]">
+        </bm-copyright>
+        <bm-map-type :map-types="['BMAP_NORMAL_MAP', 'BMAP_HYBRID_MAP']" anchor="BMAP_ANCHOR_TOP_LEFT"></bm-map-type>
+        <bm-geolocation anchor="BMAP_ANCHOR_BOTTOM_RIGHT" :showAddressBar="true" :autoLocation="true"></bm-geolocation>
+
     </baidu-map>
   </div>
 </template>
@@ -41,4 +48,7 @@ export default {
         margin: 0;
         padding: 0;
     }
+  #bm-div > div > div.BMap_cpyCtrl.BMap_noprint.anchorTR > span{
+      display: block !important;
+  }
 </style>
