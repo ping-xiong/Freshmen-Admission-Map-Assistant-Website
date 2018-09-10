65bf384afc8d6181cfbea0683f99e2cab07663f0
平兄 <597914752@qq.com>
2018-09-10 15:53:57 +0800

    完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <div id="app">
-    <baidu-map class="bm-view" id="bm-div" center="柳州市广西科技大学" :zoom="15" :scroll-wheel-zoom="true">
+    <baidu-map class="bm-view" id="bm-div" :center="centerPoint" :zoom="zoom" :scroll-wheel-zoom="true" @click="get_point">
         <bm-copyright style="display: block"
                 anchor="BMAP_ANCHOR_TOP_LEFT"
                 :copyright="[{id: 1, content: '<a href=\'http://tieba.baidu.com/f?kw=%B9%E3%CE%F7%BF%C6%BC%BC%B4%F3%D1%A7&fr=ala0&tpl=5\'><img src=\'./tieba2.png\' height=\'46px\'></a>'}]">
         </bm-copyright>
         <bm-geolocation anchor="BMAP_ANCHOR_TOP_RIGHT" :showAddressBar="true" :autoLocation="true" :offset="locPostion"></bm-geolocation>
         <bm-transit v-if="isNav" :start="start" :end="end" :auto-viewport="true" location="柳州市"></bm-transit>
+        <bm-tile
+                :isTransparentPng="true"
+                tileUrlTemplate="https://pingx.tech/app/maptiles/{Z}/tile-{X}_{Y}.png"
+                v-if="isWaPian"
+        >
+        </bm-tile>
+        <bm-marker :position="showPonit" animation="BMAP_ANIMATION_BOUNCE" v-if="isShowPoint">
+            <bm-label :content="showPonitName" :labelStyle="{color: 'red', fontSize : '20px'}" :offset="{width: -20, height: 30}"/>
+        </bm-marker>
     </baidu-map>
-    <toolbar v-on:changNavBychild="changNavBychild"></toolbar>
+      <Tooltip placement="left" content="切换卫星图层" id="close-layer" class="top_left_menu_type" :always="true" :transfer="true" :disabled="toolTipDisable">
+          <i class="iconfont" style="color: #795548" @click="close_layer">&#xe603;</i>
+      </Tooltip>
+      <Tooltip content="航拍全景" class="top_left_menu_type" id="quanjing" placement="left" :always="true" :transfer="true" :disabled="toolTipDisable">
+          <a href="https://720yun.com/t/e8f2dqO6jcg?scene_id=1106286"><img src="./assets/p.png" height="29" alt="航拍"></a>
+      </Tooltip>
+      <Tooltip content="加入贴吧官方新生群" class="top_left_menu_type" id="QQ" placement="left" :always="true" :transfer="true" :disabled="toolTipDisable" >
+          <img src="./assets/qq.png" height="28" alt="QQ群"  @click="QQqunModol = true">
+          <!--<p>贴吧官方新生群：808843028</p>-->
+      </Tooltip>
+    <toolbar v-on:changNavBychild="changNavBychild" v-on:showPoint="showPoint"></toolbar>
       <div id="bottom"></div>
+      <Modal v-model="QQqunModol">
+          <p slot="header" style="text-align:center">
+              <span>广科大贴吧2018新生群</span>
+          </p>
+          <div style="text-align:center">
+              <img src='./assets/qun.jpg' style="width: 200px;">
+              <p style="font-size: 20px; color: red">群号：808843028</p>
+          </div>
+          <div slot="footer">
+              <Button type="default" size="large" long  @click="join_QQqun">确认</Button>
+          </div>
+      </Modal>
+
   </div>
 </template>
 
 <script>
 import 'iview/dist/styles/iview.css';
 import Toolbar from './components/toolbar'
+import {Tooltip, Modal, Button} from "iview"
 
+// tileUrlTemplate="tiles/{Z}/tile{X}_{Y}.png"
+// 中点坐标: 109.452338,24.335764
 
 export default {
   name: 'app',
-    components: {Toolbar},
+    components: {Toolbar, Tooltip, Modal, Button},
     data(){
       return{
+          centerPoint:{lng: 109.452338, lat: 24.335764},
+          zoom:16,
           clientHeight: 0,
           locPostion:{
               width:10,
@@ -30,7 +67,16 @@ export default {
           },
           start:"",
           end:"",
-          isNav:false
+          isNav:false,
+          isWaPian:true,
+          isShowPoint:false,
+          showPonit:{
+              lng:0,
+              lat:0
+          },
+          showPonitName:"",
+          toolTipDisable:false,
+          QQqunModol:false,
       }
     },
     mounted(){
@@ -38,6 +84,7 @@ export default {
         window.onresize = function temp() {
             this.clientHeight = `${document.documentElement.clientHeight}`;
         };
+        this.colseToolTips();
     },
     watch:{
         clientHeight: function () {
@@ -56,7 +103,42 @@ export default {
             } else{
                 this.isNav = true;
             }
-        }
+        },
+        close_layer(){
+            // 关闭瓦片地图
+            if (this.isWaPian){
+                this.isWaPian = false;
+            } else{
+                this.isWaPian = true;
+            }
+        },
+        get_point(e){
+            // 获取坐标
+            console.log(e.point);
+            // alert(point);
+        },
+        showPoint(point, name){
+            this.showPonit = point;
+            this.centerPoint = point;
+            this.zoom = 17;
+            this.showPonitName = name;
+            this.isShowPoint = true;
+        },
+        colseToolTips(){
+            // 5秒后隐藏
+            setTimeout(this.setToolTipsFalse, 5000);
+        },
+        setToolTipsFalse(){
+            this.toolTipDisable = "disabled";
+        },
+        join_QQqun(){
+            if (this.QQqunModol){
+                this.QQqunModol = false;
+            } else{
+                this.QQqunModol = false;
+            }
+
+        },
     }
 }
 </script>
@@ -72,4 +154,57 @@ export default {
   #bm-div > div > div.BMap_cpyCtrl.BMap_noprint.anchorTL > span{
       display: block !important;
   }
+  @font-face {
+      font-family: 'iconfont';  /* project id 829567 */
+      src: url('//at.alicdn.com/t/font_829567_q760qhwhmk.eot');
+      src: url('//at.alicdn.com/t/font_829567_q760qhwhmk.eot?#iefix') format('embedded-opentype'),
+      url('//at.alicdn.com/t/font_829567_q760qhwhmk.woff') format('woff'),
+      url('//at.alicdn.com/t/font_829567_q760qhwhmk.ttf') format('truetype'),
+      url('//at.alicdn.com/t/font_829567_q760qhwhmk.svg#iconfont') format('svg');
+  }
+
+  .iconfont{
+      font-family:"iconfont" !important;
+      font-size:18px;font-style:normal;
+      -webkit-font-smoothing: antialiased;
+      -webkit-text-stroke-width: 0.2px;
+      -moz-osx-font-smoothing: grayscale;}
+  .top_left_menu_type{
+      width: 32px;
+      height: 32px;
+      cursor: pointer;
+      position: absolute;
+      margin: 0px;
+      box-sizing: border-box;
+      border: 1px solid #d9d7d5;
+      border-radius: 3px;
+      -webkit-box-shadow: 1px 1px 1px rgba(0,0,0,.2);
+      overflow: hidden;
+      text-align: center;
+      background: #f7f7f7;
+  }
+    #close-layer{
+        right: 12px;
+        top: 62px;
+        padding-top: 2px;
+    }
+    #quanjing{
+        right: 12px;
+        top: 102px;
+    }
+    #QQ{
+        right: 12px;
+        top:142px;
+        padding-top: 2px;
+    }
+    .vueBox{
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        z-index: 10;
+        height: 30px;
+        overflow: hidden;
+        background: #fff;
+    }
 </style>
--- src/components/toolbar.vue
+++ src/components/toolbar.vue
@@ -35,17 +35,13 @@
                 <!--</Button>-->
             <!--</li>-->
         <!--</ul>-->
-        <Dropdown placement="top" trigger="click">
+        <Dropdown placement="top" trigger="click" @on-click="draw_point">
             <Button>
                 <i class="iconfont" style="color: red">&#xe610;</i>
                 新生导航
             </Button>
             <DropdownMenu slot="list">
-                <DropdownItem>驴打滚</DropdownItem>
-                <DropdownItem>炸酱面</DropdownItem>
-                <DropdownItem disabled>豆汁儿</DropdownItem>
-                <DropdownItem>冰糖葫芦</DropdownItem>
-                <DropdownItem divided>北京烤鸭</DropdownItem>
+                <DropdownItem name="freshman">报到点</DropdownItem>
             </DropdownMenu>
         </Dropdown>
         <Dropdown placement="top" trigger="click">
@@ -54,24 +50,68 @@
                 学生公寓
             </Button>
             <DropdownMenu slot="list">
-                <DropdownItem>驴打滚</DropdownItem>
-                <DropdownItem>炸酱面</DropdownItem>
-                <DropdownItem disabled>豆汁儿</DropdownItem>
-                <DropdownItem>冰糖葫芦</DropdownItem>
-                <DropdownItem divided>北京烤鸭</DropdownItem>
+                <DropdownItem>
+                    <Dropdown placement="top" @on-click="draw_point">
+                        <DropdownItem>
+                            南区
+                            <Icon type="ios-arrow-forward"></Icon>
+                        </DropdownItem>
+                        <DropdownMenu slot="list">
+                            <DropdownItem name="nan_1">南1#</DropdownItem>
+                            <DropdownItem name="nan_2">南2#</DropdownItem>
+                            <DropdownItem name="nan_3">南3#</DropdownItem>
+                            <DropdownItem name="nan_4">南4#</DropdownItem>
+                            <DropdownItem name="nan_5">南5#</DropdownItem>
+                            <DropdownItem name="nan_6">南6#</DropdownItem>
+                            <DropdownItem name="nan_7">南7#</DropdownItem>
+                            <DropdownItem name="nan_8">南8#</DropdownItem>
+                            <DropdownItem name="nan_9">南9#</DropdownItem>
+                            <DropdownItem name="nan_10">南10#</DropdownItem>
+                            <DropdownItem name="nan_11">南11#</DropdownItem>
+                            <DropdownItem name="nan_12">南12#</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                </DropdownItem>
+                <DropdownItem>
+                    <Dropdown placement="top" @on-click="draw_point">
+                        <DropdownItem>
+                            北区
+                            <Icon type="ios-arrow-forward"></Icon>
+                        </DropdownItem>
+                        <DropdownMenu slot="list">
+                            <DropdownItem name="bei_1">北1#</DropdownItem>
+                            <DropdownItem name="bei_2">北2#</DropdownItem>
+                            <DropdownItem name="bei_3">北3#</DropdownItem>
+                            <DropdownItem name="bei_4">北4#</DropdownItem>
+                            <DropdownItem name="bei_5">北5#</DropdownItem>
+                            <DropdownItem name="bei_6">北6#</DropdownItem>
+                            <DropdownItem name="bei_7">北7#</DropdownItem>
+                            <DropdownItem name="bei_8">北8#</DropdownItem>
+                            <DropdownItem name="bei_9">北9#</DropdownItem>
+                            <DropdownItem name="bei_10">北10#</DropdownItem>
+                            <DropdownItem name="bei_11">北11#</DropdownItem>
+                            <DropdownItem name="bei_12">北12#</DropdownItem>
+                            <DropdownItem name="bei_13">北13#</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                </DropdownItem>
             </DropdownMenu>
         </Dropdown>
-        <Dropdown placement="top" trigger="click">
+        <Dropdown placement="top" trigger="click"  @on-click="draw_point">
             <Button>
                 <i class="iconfont" style="color: #ffc107">&#xe600;</i>
                 教学实验
             </Button>
             <DropdownMenu slot="list">
-                <DropdownItem>驴打滚</DropdownItem>
-                <DropdownItem>炸酱面</DropdownItem>
-                <DropdownItem disabled>豆汁儿</DropdownItem>
-                <DropdownItem>冰糖葫芦</DropdownItem>
-                <DropdownItem divided>北京烤鸭</DropdownItem>
+                <DropdownItem name="teach_1">第一教学楼</DropdownItem>
+                <DropdownItem name="teach_2">第二教学楼</DropdownItem>
+                <DropdownItem name="teach_3">第三教学楼</DropdownItem>
+                <DropdownItem name="teach_4">第四教学楼</DropdownItem>
+                <DropdownItem name="teach_5">第五教学楼</DropdownItem>
+                <DropdownItem name="teach_6">第六教学楼</DropdownItem>
+                <DropdownItem name="teach_7">基础实验楼</DropdownItem>
+                <DropdownItem name="teach_8">图书馆</DropdownItem>
+                <DropdownItem name="teach_9">行政楼</DropdownItem>
             </DropdownMenu>
         </Dropdown>
         <Dropdown placement="top" trigger="click" @on-click="nav">
@@ -86,7 +126,7 @@
                 <DropdownItem name="汽车站-柳石校区">汽车站-柳石校区</DropdownItem>
                 <DropdownItem divided name="飞机场-东环校区">飞机场-东环校区</DropdownItem>
                 <DropdownItem name="飞机场-柳石校区">飞机场-柳石校区</DropdownItem>
-                <DropdownItem divided name="" style="color: red">关闭路径规划</DropdownItem>
+                <DropdownItem divided name="" v-if="isNav" style="color: red">关闭路径规划</DropdownItem>
             </DropdownMenu>
         </Dropdown>
         <Anchor class="more-nav ivu-btn ivu-btn-dashed" affix="false" v-if="isNav">
@@ -96,7 +136,7 @@
 </template>
 
 <script>
-    import {Button, Dropdown, DropdownMenu, DropdownItem, Anchor, AnchorLink} from 'iview'
+    import {Button, Dropdown, DropdownMenu, DropdownItem, Anchor, AnchorLink, Icon} from 'iview'
 export default {
   name: 'toolbar',
     data(){
@@ -121,7 +161,189 @@ export default {
             lng:109.447001,
               lat:24.249786
           },
-          isNav:false
+          isNav:false,
+          nanqu:[
+              {
+                lng: 109.455379,
+                  lat:24.337027,
+                  name:"南1#"
+              },
+              {
+                  lng: 109.455572,
+                  lat:24.336731,
+                  name:"南2#"
+              },
+              {
+                  lng: 109.454768,
+                  lat:24.336756,
+                  name:"南3#"
+              },
+              {
+                  lat: 24.337287,
+                  lng: 109.455244,
+                  name:"南4#"
+              },
+              {
+                  lat: 24.337003,
+                  lng: 109.454494,
+                  name:"南5#"
+              },
+              {
+                  lat: 24.336752,
+                  lng: 109.454193,
+                  name:"南6#"
+              },
+              {
+                  lat: 24.336418,
+                  lng: 109.454934,
+                  name:"南7#"
+              },
+              {
+                  lat: 24.336143,
+                  lng: 109.454979,
+                  name:"南8#"
+              },
+              {
+                  lat: 24.33613,
+                  lng: 109.455617,
+                  name:"南9#"
+              },
+              {
+                  lat: 24.336447,
+                  lng: 109.455621,
+                  name:"南10#"
+              },
+              {
+                  lat: 24.336431,
+                  lng: 109.454278,
+                  name:"南11#"
+              },
+              {
+                  lat: 24.336122,
+                  lng: 109.454283,
+                  name:"南12#"
+              }
+          ],
+          beiqu:[
+              {
+                  lat: 24.341246,
+                  lng: 109.452383,
+                  name:"北1#"
+              },
+              {
+                  lat: 24.340464,
+                  lng: 109.452262,
+                  name:"北2#"
+              },
+              {
+                  lat: 24.340834,
+                  lng: 109.452805,
+                  name:"北3#"
+              },
+              {
+                  lat: 24.34074,
+                  lng: 109.45179,
+                  name:"北4#"
+              },
+              {
+                  lat: 24.341077,
+                  lng: 109.451795,
+                  name:"北5#"
+              },
+              {
+                  lat: 24.340443,
+                  lng: 109.451741,
+                  name:"北6#"
+              },
+              {
+                  lat: 24.338797,
+                  lng: 109.452576,
+                  name:"北7#"
+              },
+              {
+                  lat: 24.339151,
+                  lng: 109.452549,
+                  name:"北8#"
+              },
+              {
+                  lat: 24.339398,
+                  lng: 109.452423,
+                  name:"北9#"
+              },
+              {
+                  lat: 24.339987,
+                  lng: 109.451804,
+                  name:"北10#"
+              },
+              {
+                  lat: 24.339168,
+                  lng: 109.451889,
+                  name:"北11#"
+              },
+              {
+                  lat: 24.339468,
+                  lng: 109.453142,
+                  name:"北12#"
+              },
+
+              {
+                  lat: 24.339196,
+                  lng: 109.453155,
+                  name:"北13#"
+              }
+          ],
+          freshman:{
+              name:"报到点",
+              lat: 24.333797,
+              lng: 109.455294
+          },
+          teach:[
+              {
+                  lat: 24.339826,
+                  lng: 109.448067,
+                  name:"第一教学楼"
+              },
+              {
+                  lat: 24.340768,
+                  lng: 109.447927,
+                  name:"第二教学楼"
+              },
+              {
+                  lat: 24.334373,
+                  lng: 109.454301,
+                  name:"第三教学楼"
+              },
+              {
+                  lat: 24.335237,
+                  lng: 109.455293,
+                  name:"第四教学楼"
+              },
+              {
+                  lat: 24.333039,
+                  lng: 109.456178,
+                  name:"第五教学楼"
+              },
+              {
+                  lat: 24.333011,
+                  lng: 109.454368,
+                  name:"第六教学楼"
+              },
+              {
+                  lat: 24.33227,
+                  lng: 109.454346,
+                  name:"基础实验楼"
+              },
+              {
+                  lat: 24.340303,
+                  lng: 109.450923,
+                  name:"图书馆"
+              },
+              {
+                  lat: 24.332303,
+                  lng: 109.456214,
+                  name:"行政楼"
+              },
+          ]
       }
     },
   props: {
@@ -133,7 +355,8 @@ export default {
         DropdownMenu,
         DropdownItem,
         Anchor,
-        AnchorLink
+        AnchorLink,
+        Icon
     },
     methods:{
         nav(name) {
@@ -172,8 +395,26 @@ export default {
             return postion;
 
         },
-        more_nav(){
-
+        draw_point(name){
+            if (name == "" || name == undefined){
+                return;
+            }
+            // 获取点
+            let arr = name.split("_");
+            let point = null;
+            // console.log(arr[0]+","+arr[1]);
+            if (arr[0] == "nan"){
+                // console.log(arr[0]);
+                point = this.nanqu[arr[1]-1];
+            } else if (arr[0] == "bei") {
+                point = this.beiqu[arr[1]-1];
+            }else if(arr[0] == "freshman"){
+                point = this.freshman;
+            }else if(arr[0] == "teach"){
+                point = this.teach[arr[1]-1];
+            }
+            // console.log(point);
+            this.$emit("showPoint",{lat:point.lat, lng:point.lng}, point.name);
         }
     },
     mounted() {
@@ -188,11 +429,11 @@ export default {
 <style scoped>
     @font-face {
         font-family: 'iconfont';  /* project id 829567 */
-        src: url('//at.alicdn.com/t/font_829567_4th9fztjgny.eot');
-        src: url('//at.alicdn.com/t/font_829567_4th9fztjgny.eot?#iefix') format('embedded-opentype'),
-        url('//at.alicdn.com/t/font_829567_4th9fztjgny.woff') format('woff'),
-        url('//at.alicdn.com/t/font_829567_4th9fztjgny.ttf') format('truetype'),
-        url('//at.alicdn.com/t/font_829567_4th9fztjgny.svg#iconfont') format('svg');
+        src: url('//at.alicdn.com/t/font_829567_q760qhwhmk.eot');
+        src: url('//at.alicdn.com/t/font_829567_q760qhwhmk.eot?#iefix') format('embedded-opentype'),
+        url('//at.alicdn.com/t/font_829567_q760qhwhmk.woff') format('woff'),
+        url('//at.alicdn.com/t/font_829567_q760qhwhmk.ttf') format('truetype'),
+        url('//at.alicdn.com/t/font_829567_q760qhwhmk.svg#iconfont') format('svg');
     }
 
     .iconfont{
@@ -216,7 +457,7 @@ export default {
     .toolbar button{
         text-align: center;
     }
-    .toolbar i{
+    .iconfont{
         display: block;
     }
     .more-nav{
